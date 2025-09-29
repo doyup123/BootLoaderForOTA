9fd78129301c723ff6adee32d484b522b314c79c
doyup123 <kireireimu74@gmail.com>
2025-09-29 22:29:25 +0900

    Delete OTA_Project_2_BOOT/SCU_Gen6 directory

--- OTA_Project_2_BOOT/SCU_Gen6/Source/3_EcuAbs/Dio/EcuAbsDio.h
+++
@@ -1,9 +0,0 @@
-#ifndef EcuAbsDio_H
-#define EcuAbsDio_H
-
-#include "Rte_Type.h"
-
-void EA_initDio(void);
-void EA_mainDio(void);
-
-#endif
--- OTA_Project_2_BOOT/SCU_Gen6/Source/3_EcuAbs/Spi/EcuAbsSpi.h
+++
@@ -1,9 +0,0 @@
-#ifndef EcuAbsSpi_H
-#define EcuAbsSpi_H
-
-#include "Rte_Type.h"
-
-void EA_initSpi(void);
-void EA_mainSpi(void);
-
-#endif
